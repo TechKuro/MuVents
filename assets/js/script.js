388f84bc52128252f141ba846daf20b7bb2e2cd0
nimscodes <princenimako28@gmail.com>
2023-02-09 18:37:05 +0000

    little touch to the ui

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,55 @@
 
 
-// $.ajax({
-//     url: 'https://api.serpstack.com/search',
-//     data: {
-//       access_key: '0a86bae76af23e3c8a1414ff86d2d367',
-//       query: 'mcdonalds'
-//     },
-//     dataType: 'json',
-//     success: function(apiResponse) {
-//       console.log("Total results:", apiResponse.search_information.total_results);
-//       apiResponse.organic_results.map(function(result, number) {
-//         console.log(number + 1 + ".", result.title);
-//       });
-//     }
-//   });
 
 const apiKey = 'r8DA5FmL3xv8O9dO4K6WpafEvzCNG3Fv';
 
-$("form").submit(function(e) {
+$("form").submit(function (e) {
     e.preventDefault(); // prevent form from submitting and refreshing the page
-  
+
     // Get the user input
+    $('#event-row').empty();
     const userInput = $("#search").val();
     const userOption = $("#options").val();
     const userDate = $("#calendar").val();
-  
+
+    
+
     $.ajax({
-      method: "GET",
-      url:`https://app.ticketmaster.com/discovery/v2/events.json?&size=3&apikey=${apiKey}&keyword=${userInput}`,
-      async: true,
-      dataType: "json",
-    }).then(function(response) {
-      console.log(response);
-  
-      for (let i = 0; i < 3; i++) {
-        const eventImage = response._embedded.events[i].images[0].url;
-        const eventName = response._embedded.events[i].name;
-        const ticketMasterURL = response._embedded.events[i].url;
-        const eventDate = response._embedded.events[i].dates.start.localDate;
-        const eventTime = response._embedded.events[i].dates.start.localTime;
+        method: "GET",
+        url: `https://app.ticketmaster.com/discovery/v2/events.json?&size=3&apikey=${apiKey}&keyword=${userInput}&start=0&rows=3`,
+        async: true,
+        dataType: "json",
+    }).then(function (response) {
+        console.log(response);
 
-        const address_path = response._embedded.events[i]._embedded.venues[0];
-        const venue_address = {
-            name: address_path.name,
-            address: address_path.address.line1,
-            post_code: address_path.postalCode,
-            city: address_path.city.name,
-            country: address_path.country.name,
-        }
-
-        $('#event-row').append(`
+        for (let i = 0; i < 3; i++) {
+            const eventImage = response._embedded.events[i].images[1].url;
+            const eventName = response._embedded.events[i].name;
+            const ticketMasterURL = response._embedded.events[i].url;
+            const eventDate = response._embedded.events[i].dates.start.localDate;
+            const eventTime = response._embedded.events[i].dates.start.localTime;
         
-            <div class="card" style="width: 500px;">
+            const address_path = response._embedded.events[i]._embedded.venues[0];
+            const venue_address = {
+                name: address_path.name,
+                address: address_path.address.line1,
+                post_code: address_path.postalCode,
+                city: address_path.city.name,
+                country: address_path.country.name,
+            }
+
+            $('#event-row').append(`
+        
+            <div class="card border-secondary mb-3" style="width: 540px;">
                 <div class="row no-gutters">
-                    <div class="col-sm-5">
-                        <img class="card-img" src="${eventImage}" alt="Event Image">
+                    <div class="col-md-4">
+                        <img style="height: 100%;" class="card-img" src="${eventImage}" alt="Event Image">
                     </div>
-                    <div class="col-sm-7">
+                    <div class="col-md-8">
                         <div class="card-body">
                             <h5 class="card-title">${eventName}</h5>
                             <p class="card-text">Date and Time: ${eventDate}, ${eventTime}</p>
-                            <address>
+                            <address class="text-muted">
                                 Venue:<br>
                                 ${venue_address.name}<br>
                                 ${venue_address.address}<br>
@@ -74,34 +63,30 @@ $("form").submit(function(e) {
             </div>
             
       `)
-        // $(`.eventImage${i + 1}`).attr("src", eventImage);
-        // $(`.eventLink${i + 1}`).attr("href", ticketMasterURL);
-        // let eventInfo = $(`<p class="text-center actName eventName">`).text(eventName);
-        // $(`.events${i + 1}`).append(eventInfo);
-    }
+        }
     });
-  });
+});
 
-  function getJoke() {
+function getJoke() {
     $.ajax({
-      url: 'https://api.chucknorris.io/jokes/random',
-      type: 'GET',
-      success: function(response) {
-        const card = `
+        url: 'https://api.chucknorris.io/jokes/random',
+        type: 'GET',
+        success: function (response) {
+            const card = `
           <div class="card text-center">
             <div class="card-body">
               <p class="card-text">${response.value}</p>
             </div>
           </div>
         `;
-        $('body').append(card);
-      },
-      error: function() {
-        console.error('Error retrieving joke');
-      }
+            $('body').append(card);
+        },
+        error: function () {
+            console.error('Error retrieving joke');
+        }
     });
-  }
-  
-  $(document).ready(function() {
+}
+
+$(document).ready(function () {
     getJoke();
-  });
+});
